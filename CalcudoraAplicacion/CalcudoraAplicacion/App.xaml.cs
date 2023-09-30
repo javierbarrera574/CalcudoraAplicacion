d0d3d83654a49b25dff0480204b7558832d6b738
javier barrera <fcbjavier12@hotmail.com>
2023-09-30 01:51:21 -0300

    Agregar archivos de proyecto.

---
+++ CalcudoraAplicacion/CalcudoraAplicacion/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace CalcudoraAplicacion
+{
+    public partial class App : Application
+    {
+        protected Operacion operacion;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage(operacion);
+        }
+        protected override void OnStart() { }
+        protected override void OnSleep() { }
+        protected override void OnResume() { }
+       
+    }
+}
---
+++ CalcudoraAplicacion/CalcudoraAplicacion/MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CalcudoraAplicacion
+{
+    public partial class MainPage : ContentPage
+    {
+        double Resultado = 0;
+
+        int ResultadoResto;
+
+        private readonly Operacion _operacion;
+
+        #region
+        //public MainPage(Operacion operacion)
+        //{
+        //    this._operacion = operacion;
+        //}
+        #endregion
+
+        public MainPage(Operacion operacion)
+        {
+            this._operacion = operacion;
+            InitializeComponent();
+            OpcionOperacion.Items.Add(Operacion.Suma.ToString());
+            OpcionOperacion.Items.Add(Operacion.Resta.ToString());
+            OpcionOperacion.Items.Add(Operacion.Multiplicacion.ToString());
+            OpcionOperacion.Items.Add(Operacion.Division.ToString());
+            OpcionOperacion.Items.Add(Operacion.Potenciacion.ToString());
+            OpcionOperacion.Items.Add(Operacion.Radicacion.ToString());
+            OpcionOperacion.Items.Add(Operacion.ValorAbsoluto.ToString());
+            OpcionOperacion.Items.Add(Operacion.Resto.ToString());
+            OpcionOperacion.Items.Add(Operacion.Exponenciacion.ToString());
+            OpcionOperacion.Items.Add(Operacion.MaximoValor.ToString());
+            OpcionOperacion.SelectedIndexChanged += OpcionOperacion_SelectedIndexChanged;
+            calcular.Clicked += Calcular_Clicked;
+        }
+
+        private void OpcionOperacion_SelectedIndexChanged(object sender, EventArgs e) { }
+        
+        private void Calcular_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNumero1.Text) && !string.IsNullOrEmpty(txtNumero2.Text))
+            {
+                String op = OpcionOperacion.SelectedItem.ToString();
+                int Numero1 = Int32.Parse(txtNumero1.Text);
+                int Numero2 = Int32.Parse(txtNumero2.Text);
+
+                switch (_operacion)
+                {
+                    case Operacion.Suma:
+                        Resultado = Numero1 + Numero2;
+                        break;
+                    case Operacion.Resta:
+                        Resultado = Numero1 - Numero2;
+                        break;
+                    case Operacion.Multiplicacion:
+                        Resultado = Numero1 * Numero2;
+                        break;
+                    case Operacion.Division:
+                        Resultado = Numero1 / Numero2;
+                        break;
+                    case Operacion.Potenciacion:
+                        Resultado = Math.Pow(Numero1, Numero2);
+                        break;
+                    case Operacion.Radicacion:
+                        Resultado = Math.Sqrt(Numero1);
+                        break;
+                    case Operacion.ValorAbsoluto:
+                        Resultado = Math.Abs(Numero1);
+                        break;
+                    case Operacion.Resto:
+                        Resultado = Math.DivRem(Numero1, Numero2, out ResultadoResto);
+                        break;
+                    case Operacion.Exponenciacion:
+                        Resultado = Math.Exp(Numero1);
+                        break;
+                    case Operacion.MaximoValor:
+                        Resultado = Math.Max(Numero1, Numero2);
+                        break;
+                    default:
+                        Console.WriteLine("No es posible realizar calculos multiples");
+                        break;
+                }
+
+                #region
+                //if (op == "suma")
+                //{
+                //    Resultado = Numero1 + Numero2;
+                //}
+                //else if (op == "resta")
+                //{
+                //    Resultado = Numero1 - Numero2;
+                //}
+                //else if (op == "multiplicacion")
+                //{
+                //    Resultado = Numero1 * Numero2;
+                //}
+                //else if (op == "division")
+                //{
+                //    Resultado = Numero1 / Numero2;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("no se pueden hacer calculos multiples");
+                //}
+                #endregion
+
+            }
+            //else
+            //{
+            //    DisplayAlert("Datos erroneos", "Llene los espacios", "Ok");
+            //}
+            lblResultado.Text = Resultado + "";
+        }
+    }
+    public enum Operacion
+    {
+        Suma=1,
+        Resta=2,
+        Multiplicacion=3,
+        Division=4,
+        Potenciacion=5,
+        Radicacion=6,
+        ValorAbsoluto=7,
+        Resto=8,
+        Exponenciacion=9,
+        MaximoValor=10
+    }
+}
